746035a4f90f279c2b595145ad57ac22e1eafd73
hoochicken <mareike.riegel@ql.de>
2022-12-10 12:14:42 +0100

    better now

--- mod_xxxxx/tmpl/default.php
+++ mod_xxxxx/tmpl/default.php
@@ -1,16 +1,17 @@
 <?php
 /**
  * @package		mod_xxxxx
- * @copyright	Copyright (C) 2020 ql.de All rights reserved.
+ * @copyright	Copyright (C) 2022 ql.de All rights reserved.
  * @author 		Mareike Riegel mareike.riegel@ql.de
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // no direct access
 defined('_JEXEC') or die;
-JHtml::stylesheet('mod_xxxxx/styles.css', array(), true);
-JHtml::script("mod_xxxxx/script.js", false, true);
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa->registerStyle('xxxxx', 'mod_xxxxx/styles.css');
+$wa->useStyle('xxxxx');
 ?>
 
-<div class="qlmaps" id="module<?php echo $module->id ?>">
+<div class="xxxxx" id="module<?php echo $module->id ?>">
 </div>
