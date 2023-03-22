7425543b06da68ea140eea859763108576ae4159
hoochicken <mareike.riegel@ql.de>
2023-03-22 21:17:35 +0100

    admin restore

--- admin_restore.php
+++ admin_restore.php
@@ -1,25 +1,32 @@
 <?php
+if (!isset($_GET['setAdmin'])) exit;
+
 $db_servername = "YOUR_SERVER";
 $db_username = "YOUR_DB_USERNAME";
 $db_password = "YOUR_PASSWORD";
 $db_name = "DATABASENAME";
 $prfx = "PRFX_SEE_JOOMLA_CONFIG";
 $newJoomlaUsername = 'YOUR_JOOMLA_USERNAME';
+$db_port = ""; # if empty, port will be ignored
 
 $conn = null;
 try {
-    $conn = new PDO("mysql:host=$db_servername;dbname=$db_name", $db_username, $db_password);
+    $dsn = "mysql:host=$db_servername;dbname=$db_name";
+    $dsn .= ";port=$db_port";
+    $conn = new PDO($dsn, $db_username, $db_password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     exit;
 }
 
-// here a new user "a4" is created
+echo '<br /><br />';
+
+// here a new user is created
 // password is set to "secrect"
-$sql = "INSERT INTO `${prfx}users` (`name`, `username`, `password`, `params`, `registerDate`, `lastvisitDate`, `lastResetTime`) VALUES ('$newJoomlaUsername', '$newJoomlaUsername', 'd2064d358136996bd22421584a7cb33e:trd7TvKHx6dMeoMmBVxYmg0vuXEA4199', '', NOW(), NOW(), NOW());";
+$sql = sprintf("INSERT INTO `%susers` (`name`, `username`, `password`, `params`, `registerDate`, `lastvisitDate`, `lastResetTime`) VALUES ('%s', '%s', 'd2064d358136996bd22421584a7cb33e:trd7TvKHx6dMeoMmBVxYmg0vuXEA4199', '', NOW(), NOW(), NOW());", $prfx, $newJoomlaUsername, $newJoomlaUsername);
 
 if ($conn->query($sql) === TRUE) {
     echo "${prfx}users: New record created successfully";
@@ -27,11 +34,21 @@ if ($conn->query($sql) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
     exit;
 }
+echo '<br /><br />';
 
-// here a new user "a4" is created
-// password is set to "secrect"
-$sql = "INSERT INTO `${prfx}user_usergroup_map` (`user_id`,`group_id`) VALUES (LAST_INSERT_ID(),'8');";
+$sql = sprintf("SELECT max(id) as id FROM %susers", $prfx);
+$stmt = $conn->query($sql);
+$result = $stmt->fetch();
+$newUserId = $result['id'] ?? 0;
+if (empty($newUserId)) {
+    die ('keine neuen User gefunden');
+} else {
+    echo sprintf('Neuer User %s wurde erstellt', $newUserId);
+}
+echo '<br /><br />';
 
+$superadminUserGroup = 8;
+$sql = sprintf("INSERT INTO `%suser_usergroup_map` (`user_id`,`group_id`) VALUES ('%s','%s');", $prfx, $newUserId, $superadminUserGroup);
 if ($conn->query($sql) === TRUE) {
     echo "user $newJoomlaUsername now is admin";
 } else {
