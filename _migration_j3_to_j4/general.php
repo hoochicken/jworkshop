6fe957d44b9543cd9879d336fe99a16d1055c66b
Hoochicken <mareike.riegel@ql.de>
2023-06-21 15:41:38 +0200

    dispatcher

--- _migration_j3_to_j4/general.php
+++ _migration_j3_to_j4/general.php
@@ -25,3 +25,22 @@ $obj_qlproto->aside ?? 'div'
   
 # ????
 echo $this->pagination->getPagesCounter(); 
+
+
+
+$dispatcher = Joomla\CMS\Factory::getApplication()->getDispatcher();
+
+$event = new Joomla\Event\Event('onContentPrepare', [$this->category->extension . '.categories', &$this->category, &$this->params, 0]);
+$res = $dispatcher->dispatch('onContentPrepare', $event);
+
+$event = new Joomla\Event\Event('onContentAfterTitle', [$this->category->extension . '.categories', &$this->category, &$this->params, 0]);
+$results = $dispatcher->dispatch('onContentAfterTitle', $event)->getArgument('result');
+$afterDisplayTitle = trim(implode("\n", $results));
+
+$event = new Joomla\Event\Event('onContentBeforeDisplay', [$this->category->extension . '.categories', &$this->category, &$this->params, 0]);
+$results = $dispatcher->dispatch('onContentBeforeDisplay', $event)->getArgument('result');
+$beforeDisplayContent = trim(implode("\n", $results));
+
+$event = new Joomla\Event\Event('onContentAfterDisplay', [$this->category->extension . '.categories', &$this->category, &$this->params, 0]);
+$results = $dispatcher->dispatch('onContentAfterDisplay', $event)->getArgument('result');
+$afterDisplayContent = trim(implode("\n", $results));
